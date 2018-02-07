8c82adfd8c5d8c506423fb6c38434fc1f0669829
H <50N40W@users.noreply.github.com>
2018-02-06 19:40:13 -0500

    Update arco.py
    20180206 Lesson 4 code changes

--- arco.py
+++ arco.py
@@ -12,6 +12,17 @@ def openFile():
     print ("openFile")
     with open('arco.csv') as csvfile:
         myCSV=csv.reader(csvfile, delimiter=',')
+        areacodes={}
+        i=0
+        for row in myCSV:
+            if(row[3]):
+                area = row[0]
+                city = row[3]
+                areacodes[i]=(str(area)+","+row[1]+' '+city)
+                print(areacodes[i])
+            i=i+1
+    return areacodes
+
 #search data for area code
 def searchData():
     print ("searchData")
